6df492303556227e4edc0f79f47ffbbc1c6af910
Hannes Rydén <1542042+Hannessa@users.noreply.github.com>
2018-05-29 19:32:58 +0200

    First version

---
+++ example_bot.py
@@ -0,0 +1,8 @@
+import sc2
+
+class ExampleBot(sc2.BotAI):
+    async def on_step(self, iteration):
+    	# On first step, send all workers to attack enemy start location
+        if iteration == 0:
+            for worker in self.workers:
+                await self.do(worker.attack(self.enemy_start_locations[0]))
---
+++ run.py
@@ -0,0 +1,22 @@
+import sc2, sys
+from __init__ import run_ladder_game
+from sc2 import Race, Difficulty
+from sc2.player import Bot, Computer
+
+# Load bot
+from example_bot import ExampleBot
+bot = Bot(Race.Random, ExampleBot())
+
+# Start game
+if __name__ == '__main__':
+	if "--LadderServer" in sys.argv:
+		# Ladder game started by LadderManager
+		print("Starting ladder game...")
+		run_ladder_game(bot)
+	else:
+		# Local game
+		print("Starting local game...")
+		sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
+			bot,
+			Computer(Race.Protoss, Difficulty.VeryHard)
+		], realtime=True)
