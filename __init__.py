9155d95e7d1e27c659e79232bc6b0dfd8039649e
Daniel Köhntopp <33926917+Archiatrus@users.noreply.github.com>
2018-11-14 22:42:57 +0100

    Do not use leaveGame or quit with the ladderManager

--- __init__.py
+++ __init__.py
@@ -65,24 +65,23 @@ def run_ladder_game(bot):
 
     # Run it
     result = asyncio.get_event_loop().run_until_complete(g)
-    print(result)
+    return result, args.OpponentId
 
 # Modified version of sc2.main._join_game to allow custom host and port, and to not spawn an additional sc2process (thanks to alkurbatov for fix)
 async def join_ladder_game(host, port, players, realtime, portconfig, save_replay_as=None, step_time_limit=None, game_time_limit=None):
     ws_url = "ws://{}:{}/sc2api".format(host, port)
     ws_connection = await aiohttp.ClientSession().ws_connect(ws_url, timeout=120)
     client = Client(ws_connection)
-
     try:
         result = await sc2.main._play_game(players[0], client, realtime, portconfig, step_time_limit, game_time_limit)
         if save_replay_as is not None:
             await client.save_replay(save_replay_as)
-        await client.leave()
-        await client.quit()
+        #await client.leave()
+        #await client.quit()
     except ConnectionAlreadyClosed:
         logging.error(f"Connection was closed before the game ended")
         return None
     finally:
-        ws_connection.close()
+        await ws_connection.close()
 
     return result
